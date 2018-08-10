8d756bbc3fe772cf00ddc7d177e813841e6cb8f0
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-11 02:55:36 +0300

    add form to form.component template

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,9 +6,12 @@ import {Quote} from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  title = 'Quotes';
+  title = 'Quotes Generating App!';
   quotes = [
-    new Quote(1., 'feysal', 'PASSION LED US HERE' ),
+    new Quote(1, 'Feysal', 'PASSION LEAD US HERE'),
+    new Quote(1, 'Feysal', 'PASSION LEAD US HERE'),
+    new Quote(1, 'Feysal', 'PASSION LEAD US HERE'),
+    new Quote(1, 'Feysal', 'PASSION LEAD US HERE'),
   ];
 
   constructor() { }
