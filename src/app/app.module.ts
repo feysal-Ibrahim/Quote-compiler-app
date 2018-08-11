e58c3250907e32bbd20d6085d259a9fd2d17e865
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-11 17:11:51 +0300

    edited the readme file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    QuoteFormComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
